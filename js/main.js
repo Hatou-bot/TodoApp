b546829027c1672d1d7759a6c0e77e9a29d40b0c
pgarcia92 <palomaglorenzo@gmail.com>
2020-05-01 17:59:22 +0200

    Add priorities

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const taskList = [{'taskId': 101, 'Name':'Studying Javascript', 'Priority':'Urgent'},
             {'taskId': 102, 'Name':'Polishing the 4th wall', 'Priority': 'Daily'},
-            {'taskId': 103, 'Name':'Dethroning unjust kings', 'Priority:':'Weekly'},
+            {'taskId': 103, 'Name':'Dethroning unjust kings', 'Priority':'Weekly'},
             {'taskId': 104, 'Name':'Pondering on the meaning of life, the universe, and everything else', 'Priority':'Monthly'},
 ]
 
 const taskBody = document.querySelector('.task-body')
 const taskInput = document.querySelector('.add-task')
 const clickTask = document.querySelector('.first-bar button')
+const priority = document.querySelector('.first-bar select')
 
 function printTasks(pTasks){ //prints tasks in the body
     taskBody.innerHTML = ""
@@ -14,6 +15,7 @@ function printTasks(pTasks){ //prints tasks in the body
     for(const task of pTasks){
         taskBody.innerHTML += `<article>
         <p>${task.Name}</p>
+        <p>${task.Priority}</p>
         <button class="delete">Eliminar</button>
     </article>`
     }
@@ -21,15 +23,16 @@ function printTasks(pTasks){ //prints tasks in the body
 printTasks(taskList)
 //printTasks(taskList)
 
-clickTask.addEventListener('click', onSaveTask)
+clickTask.addEventListener('click', onSaveTask) //assigns onSaveTask function to button
 var taskId = 0
 function onSaveTask(){
     const task = {
         taskId: taskId,
         Name: taskInput.value,
-        Priority: "Daily",
+        Priority: priority.value,
     }
     taskList.push(task)
     taskId++;
+    taskInput.value = ""
     printTasks(taskList)
 }
